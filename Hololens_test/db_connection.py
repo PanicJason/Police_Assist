87b4bbf25f7022faf63db49393bf7cb6164da347
jason <skyhumteok@naver.com>
2023-09-17 20:23:56 +0900

    Update db_connection.py to use JAWSDB_URL

--- Hololens_test/db_connection.py
+++ Hololens_test/db_connection.py
@@ -1,12 +1,39 @@
+import os
+import urllib.parse
 import pymysql
 
 def connect_db():
-    # Change the host, user, password, and database accordingly
-    connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='1234',
-                                 database='police_test')
+    database_url = os.environ.get('JAWSDB_URL')
+
+    if database_url:
+        url = urllib.parse.urlparse(database_url)
+        db_name = url.path[1:]
+        db_user = url.username
+        db_password = url.password
+        db_host = url.hostname
+        db_port = url.port
+
+        connection = pymysql.connect(host=db_host,
+                                     port=db_port,
+                                     user=db_user,
+                                     password=db_password,
+                                     database=db_name)
+    else:
+        # Fallback to local database configuration if DATABASE_URL is not set
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='1234',
+                                     database='police_test')
+
     return connection
+
+# def connect_db():
+#     # Change the host, user, password, and database accordingly
+#     connection = pymysql.connect(host='localhost',
+#                                  user='root',
+#                                  password='1234',
+#                                  database='police_test')
+#     return connection
 
 def get_data_from_assist_test(file_name=None):
     conn = connect_db()
