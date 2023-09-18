cca0fc0024584427aa6fcce3194dc6a045646436
jason <skyhumteok@naver.com>
2023-09-18 15:26:32 +0900

    Fix db_con

--- Hololens_test/db_connection.py
+++ Hololens_test/db_connection.py
@@ -27,16 +27,12 @@ def connect_db():
 
     return connection
 
-# def connect_db():
-#     # Change the host, user, password, and database accordingly
-#     connection = pymysql.connect(host='localhost',
-#                                  user='root',
-#                                  password='1234',
-#                                  database='police_test')
-#     return connection
-
 def get_data_from_assist_test(file_name=None):
     conn = connect_db()
+    # 추가
+    if conn is None:
+        return None
+    
     try:
         with conn.cursor(pymysql.cursors.DictCursor) as cursor:
             if file_name:
@@ -46,12 +42,20 @@ def get_data_from_assist_test(file_name=None):
                 sql = "SELECT * FROM assist_test"
                 cursor.execute(sql)
             result = cursor.fetchall()
+        return result
+    #추가
+    except Exception as e:
+        print(f"An error occurred while executing the SQL query: {e}")
+        return None
+    
     finally:
         conn.close()
-    return result
+    
 
 # Example usage
 if __name__ == '__main__':
     data = get_data_from_assist_test()
-    for row in data:
-        print(row)
+    # 추가
+    if data:
+        for row in data:
+            print(row)
